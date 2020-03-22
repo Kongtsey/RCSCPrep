c5ed50da63eb9d7db38a7d2a8d9fb11910fbbc42
kzoepa <kzoepa@gmail.com>
2020-03-22 18:09:50 +0600

    converted the question and choice into JSON format and is stored in PEQuestion.txt

--- JSONConverter/classifierQuestions.py
+++ JSONConverter/classifierQuestions.py
@@ -1,8 +1,19 @@
 def remover(list,x):
+    """
+    :param list: the list you want to remove stuff out of
+    :param x: the element you want to remove
+    :return: list without any x elements
+    """
     while x in list:
         list.remove(x)
     return list
 def finder(list,x,qNum):
+    """
+    :param list: list with all of the text of the questions
+    :param x: What character to find
+    :param qNum: The question number to stop at
+    :return: anotherList: returns list with only question and choices as elements
+    """
     anotherList = []
     for i in range(len(list)):
         if list[i][0:2]==x:
@@ -19,7 +30,13 @@ def finder(list,x,qNum):
                     pass
     return (anotherList)
 def finder2(list,x,qNum):
-    anotherList = []
+    """
+
+    :param list: list with all of the questions and choices
+    :param x: argument where the question ends
+    :param qNum: which question to be stored
+    :return: temp: just the question concerned with the qNum
+    """
     for i in range(len(list)):
         if list[i][0:2]==x:
             temp = []
@@ -27,34 +44,51 @@ def finder2(list,x,qNum):
                 temp.append(list[y])
                 try:
                     if(int(list[y][0:2])==qNum):
-                        int(list[y][0:2])
-                        anotherList.append(temp)
-                        qNum+=1
-                        return (temp,True)
+                        return (temp)
                 except:
                     pass
 def finder3(list,x):
-    anotherList = []
+    """
+
+    :param list: list with all of the questions and choices
+    :param x: argument where the choices begin
+    :return: only the choices for each question.
+    """
     for i in range(len(list)):
         if list[i][0:2]==x:
             temp = []
             for y in range(i,-1,-1):
                 temp.append(list[y])
                 if list[y][0:2] == 'A.':
-                    anotherList.append(temp)
                     return (temp)
 def reverse(list):
+    """
+
+    :param list: list to reverse
+    :return: anotherList: list with all elements reversed
+    """
     anotherList = []
     for i in range(len(list)-1,-1,-1):
         anotherList.append(list[i])
     return anotherList
 def iterator(list, method):
+    """
+
+    :param list: list to perform method on
+    :param method: method to perform, performs method on each element of list
+    :return: anotherList: list with each of the element that has the method done on
+    """
     anotherList = []
     for i in range(len(list)):
         temp = method(list[i])
         anotherList.append(temp)
     return (anotherList)
 def joiner(list):
+    """
+
+    :param list: just a list with elements
+    :return: fullThing: combines all element of list in one string and returns it.
+    """
     fullThing = ""
     for i in range(len(list)):
         fullThing += str(list[i])
--- JSONConverter/script.py
+++ JSONConverter/script.py
@@ -3,31 +3,34 @@ import json as js
 prblmSlvPage = open("PastPapers/2010_PE.txt","r")
 content = prblmSlvPage.read()
 content = content.split("\n")
-questions = []
-choices = []
-content = cQ.remover(content,"")
-qNum = 51
-content = cQ.finder(content,'D.',qNum)
-orderedList = []
-orderedList = cQ.iterator(content, cQ.reverse)
+# split all of the text files by new lines.
+questions = [] # list to store all the questions
+choices = [] # list to store all the choices
+content = cQ.remover(content,"") # removes the blank elements in the content list
+qNum = 51 # since the questions start from number 51
+content = cQ.finder(content,'D.',qNum) # updates the content list to contain each question and choice as a single element in the list
+orderedList = [] # list to store content but in a standard way
+orderedList = cQ.iterator(content, cQ.reverse) # reverses the list content
 for i in range(len(orderedList)):
-    questionResult = cQ.finder2(orderedList[i],'A.',qNum)
-    if questionResult[1]==True:
-        questions.append(questionResult[0])
-        qNum+=1
-questions = cQ.iterator(questions, cQ.reverse)
-questions = cQ.iterator(questions, cQ.joiner)
+    questionResult = cQ.finder2(orderedList[i],'A.',qNum)# function to find questions since some of them have new lines so are broken down as new questions.
+    questions.append(questionResult)
+    qNum+=1
+questions = cQ.iterator(questions, cQ.reverse) # just reverses in order to have it in ascending order
+questions = cQ.iterator(questions, cQ.joiner) # combines each part of the question as a whole since some are broken down due to txt errors
 for i in range(len(orderedList)):
-    choicesResult = cQ.finder3(orderedList[i], 'D.')
+    choicesResult = cQ.finder3(orderedList[i],'D.') # compiles all choices into a single list and have each choice as an element
     choices.append(choicesResult)
 choices = cQ.iterator(choices,cQ.reverse)
 
 # Time to convert into a dictionary
 questionsAnswers = {
-    "questions":{},
-    "choices":{}
+    "questions":"",
+    "choices":""
 }
+json_file = open('PastPapers/PEQuestion.txt','w')
 for i in range(len(choices)):
-    questionsAnswers["questions"][i+1]=questions[i]
-    questionsAnswers["choices"][i+1]=choices[i]
-print(questionsAnswers)
+    questionsAnswers["questions"]=questions[i]
+    questionsAnswers["choices"]=choices[i]
+    data = js.dumps(questionsAnswers)
+    json_file.write(data+"\n")
+json_file.close()
