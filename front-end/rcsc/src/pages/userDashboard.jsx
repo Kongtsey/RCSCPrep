a9e685e13c66aae86903c45f9faf4e38af95ec31
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2020-04-29 00:00:55 +0600

    added the dashboard tool bar

--- front-end/rcsc/src/pages/userDashboard.jsx
+++ front-end/rcsc/src/pages/userDashboard.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import GeneralNavigationBar from "../components/generalNavbar";
+import { Container, Row, Col } from "react-bootstrap";
+import DashboardTool from "../components/dashboardTool";
+import "../style-sheet/user-dashboard.css";
 
 function userDashboard() {
   return (
     <React.Fragment>
       <GeneralNavigationBar />
-      <h1> Hello World </h1>
+      <Container fluid={true}>
+        <Row>
+          <Col className='dashboard-tool' md={3}>
+            <br />
+            <DashboardTool />
+          </Col>
+        </Row>
+      </Container>
     </React.Fragment>
   );
 }
