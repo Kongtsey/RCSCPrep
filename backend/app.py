622d6c13863d04818c237bea4e0ca73fa97669e0
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2020-05-21 22:11:38 +0600

    added python file

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,17 @@
-import flask
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
+# SETTING UP THE CREDENTIALS.
 cred = credentials.Certificate("./config.json")
 app = firebase_admin.initialize_app(cred)
 db = firestore.client()
-documento = db.collection(u'Questions').document(u'ques')
-doc = documento.get()
-print(u'Document data: {}'.format(doc.to_dict()))
+
+# FETCHING THE DATA
+data = db.collection(u'Questions').stream()
+for doc in data:
+    #print(u'{} => {}'.format(doc.id, doc.to_dict()))
+    print(doc.id('UserHasResponded'))
+
+
+
