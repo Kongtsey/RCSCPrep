e1074d83627061351c490abbbc037bcb53b3a96b
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2020-05-22 01:04:39 +0600

    bootstrap styling works now

--- front-end/rcsc/src/pages/UserDashboard.jsx
+++ front-end/rcsc/src/pages/UserDashboard.jsx
@@ -1,21 +1,24 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "../style-sheet/user-dashboard.css";
 import fire from "../config/Fire";
-class UserDashboard extends Component{
-  constructor(props){
-    super (props);
+
+class UserDashboard extends Component {
+  constructor(props) {
+    super(props);
     this.logout = this.logout.bind(this);
   }
-  logout(){
+  logout() {
     fire.auth().signOut();
   }
   render() {
     return (
+      <React.Fragment>
         <div>
           <h1>WELCOME USER</h1>
-          <br/>
+          <br />
           <button onClick={this.logout}>Logout</button>
         </div>
+      </React.Fragment>
     );
   }
 }
