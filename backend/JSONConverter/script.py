95acab6843024c9d90fddd1bd01d6a6f4680e8ea
kzoepa <kzoepa@gmail.com>
2020-05-23 15:12:32 +0600

    added credentials

--- backend/JSONConverter/script.py
+++ backend/JSONConverter/script.py
@@ -4,7 +4,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 # Use the application default credentials
-cred = credentials.Certificate("../Credentials.json")
+cred = credentials.Certificate("../credentials/Credentials.json")
 firebase_admin.initialize_app(cred, {
   'projectId': "bhutanexamfactory-d7ea2",
 })
