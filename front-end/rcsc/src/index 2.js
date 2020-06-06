7143493cc4c33a214f46d3b1bf60331fbb9a238a
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2020-06-06 12:42:46 +0600

    removed the duplicate copies of files

--- front-end/rcsc/src/index 2.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import Homepage from "./App";
-import * as serviceWorker from "./serviceWorker";
-
-ReactDOM.render(<Homepage />, document.getElementById("root"));
-
-serviceWorker.unregister();
