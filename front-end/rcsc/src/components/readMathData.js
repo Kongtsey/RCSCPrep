e5ab5d56e6c2060ffec4076d768c955fb3e308ae
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2020-06-10 16:46:16 +0600

    reading the data

--- front-end/rcsc/src/components/readMathData.js
+++ front-end/rcsc/src/components/readMathData.js
@@ -4,6 +4,7 @@ import { Container, Col, Row, Form } from "react-bootstrap";
 
 function MathList(props) {
   const [times, setTimes] = useState([]);
+  const [question, setQuestion] = useState([]);
   useEffect(() => {
     fire
       .firestore()
@@ -37,22 +38,36 @@ function MathList(props) {
       });
   }, [prop]);
 
-  const handleChange = (e, choice, time) => {
-    console.log("hello world: ", time, " : ", choice);
+  const handleChange = (e, choice, question_id, CorrectAnswer) => {
+    console.log("hello world: ", question_id, " : ", choice, " but the correct answer is : ", CorrectAnswer);
+    const newQuestion = () => ({
+      id: question_id,
+      user_answer: choice,
+    });
+    //Check if the state is empty or not. If it is not, then you have to check if the user is merely changing the
+    //answer to an already answered question or answering a fresh new question.
+    if (question.length === 0) {
+      console.log("This is the first entry/first user's choice");
+      setQuestion((question) => [...question, newQuestion]);
+    } else {
+      setQuestion((question) => [...question, newQuestion]);
+    }
   };
+
+  console.log("this is outside yhe fucnction:  ", question);
 
   return (
     <Container>
       <Row>
         <Col md={12} lg={12} sm={12}>
           <ol>
-            {times.map((time) => (
-              <li id={time.id}>
-                <div>{time.Question} </div>
+            {times.map((data) => (
+              <li id={data.id}>
+                <div>{data.Question} </div>
                 <Form>
-                  {time.Choice.map((choice) => (
+                  {data.Choice.map((choice) => (
                     <p>
-                      <input type='radio' name='choice' value={choice} onChange={(e) => handleChange(e, choice, time.id)} />
+                      <input type='radio' name='choice' value={choice} onChange={(e) => handleChange(e, choice, data.id, data.CorrectAnswer)} />
                       {choice}
                     </p>
                   ))}
