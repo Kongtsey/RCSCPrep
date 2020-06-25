38feab8e75fdfcb4ef4f9e3c8865d145e3197b24
kzoepa <kzoepa@gmail.com>
2020-06-25 10:13:39 +0600

    removed errors and redundant code

--- front-end/rcsc/src/components/HomepageInfo.js
+++ front-end/rcsc/src/components/HomepageInfo.js
@@ -3,10 +3,6 @@ import { Container, Col, Row } from "react-bootstrap";
 import "../style-sheet/home-page-info.css";
 import dashboard_image from "../images/dashboard.png";
 import ScrollAnimation from 'react-animate-on-scroll';
-import image_subsection_1 from "../images/subsection-1.png";
-import image_subsection_2 from "../images/subsection-2.png";
-import image_subsection_3 from "../images/subsection-3.png";
-import image_subsection_4 from "../images/subsection-4.png";
 
 function HomepageInfo() {
   return (
