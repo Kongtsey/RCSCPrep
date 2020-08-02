7ac5282b62878b7e58b9a5b2200629ed340978e3
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2020-08-02 12:35:38 +0600

    Ongoing: displaying and hiding on button click

--- front-end/rcsc/src/pages/examPractice.js
+++ front-end/rcsc/src/pages/examPractice.js
@@ -1,89 +1,58 @@
 import React, { Component } from "react";
-import { Redirect, withRouter } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import "../style-sheet/exam-practice.css";
 import { Container, Col, Row, Button } from "react-bootstrap";
 import GeneralNavbar from "../components/generalNavbar";
 import ReadExamPracticeMath from "../components/readExamPracticeMath";
+import ReadExamPracticeEnglish from "../components/readExamPracticeEnglish";
+import $ from "jquery";
+
 class ExamPractice extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleDisplay = this.handleDisplay.bind(this);
+  }
+
+  handleDisplay = () => () => {};
+
   render() {
     return (
       <React.Fragment>
         {<GeneralNavbar />}
         <Container>
           <br />
+          <Row>
+            <h3> Practice Exam </h3>
+            <hr />
+          </Row>
           <br />
-          <h3> Practice Exam </h3>
-          <hr />
-          <div className='tabs'>
-            <Tabs>
-              <Tab label='Math'>
-                <div>
-                  <ReadExamPracticeMath />
-                </div>
-              </Tab>
-              <Tab label='English'>
-                <div>
-                  <p>Tab 2 content</p>
-                </div>
-              </Tab>
-              <Tab label='Dzongkha'>
-                <div>
-                  <p>Tab 3 content</p>
-                </div>
-              </Tab>
-              <Tab label='Data Interpretation'>
-                <div>
-                  <p>Tab 4 content</p>
-                </div>
-              </Tab>
-            </Tabs>
-          </div>
+          <Row>
+            <Col md={2} id={"math"} className={"tabs active"}>
+              <button onClick={this.handleDisplay}>Math</button>
+            </Col>
+            <Col md={2} id={"english"} className={"tabs"}>
+              <button onClick={this.handleDisplay}>English</button>
+            </Col>
+            <Col md={2} id={"dzongkha"} className={"tabs"}>
+              <button onClick={this.handleDisplay}>Dzongkha</button>
+            </Col>
+            <Col md={3} id={"data"} className={"tabs"}>
+              <button onClick={this.handleDisplay}>Data Interpretation</button>
+            </Col>
+          </Row>
+          <br /> <br />
+          <Row>
+            <div className={"subjects"}>
+              <ReadExamPracticeMath />
+            </div>
+            <div className={"subjects"}>
+              <ReadExamPracticeEnglish />
+            </div>
+          </Row>
         </Container>
       </React.Fragment>
     );
   }
 }
-
-class Tabs extends React.Component {
-  state = {
-    activeTab: this.props.children[0].props.label,
-  };
-  changeTab = (tab) => {
-    this.setState({ activeTab: tab });
-  };
-  render() {
-    let content;
-    let buttons = [];
-    return (
-      <div>
-        {React.Children.map(this.props.children, (child) => {
-          buttons.push(child.props.label);
-          if (child.props.label === this.state.activeTab) content = child.props.children;
-        })}
-
-        <TabButtons activeTab={this.state.activeTab} buttons={buttons} changeTab={this.changeTab} />
-        <div className='tab-content'>{content}</div>
-      </div>
-    );
-  }
-}
-
-const TabButtons = ({ buttons, changeTab, activeTab }) => {
-  return (
-    <div className='tab-buttons'>
-      {buttons.map((button) => {
-        return (
-          <button className={button === activeTab ? "active" : ""} onClick={() => changeTab(button)}>
-            {button}
-          </button>
-        );
-      })}
-    </div>
-  );
-};
-
-const Tab = (props) => {
-  return <React.Fragment>{props.children}</React.Fragment>;
-};
 
 export default withRouter(ExamPractice);
