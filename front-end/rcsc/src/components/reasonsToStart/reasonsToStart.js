9dfcc7c75c609a888594d4caca61dd46966afab0
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2020-08-13 18:25:46 +0600

    FEATURE: added a background picture with parallax effect on the reasons why we started

--- front-end/rcsc/src/components/reasonsToStart/reasonsToStart.js
+++ front-end/rcsc/src/components/reasonsToStart/reasonsToStart.js
@@ -16,21 +16,21 @@ class ReasonsToStart extends React.Component {
         </Row>
         <Row>
           <Col sm={4} className='threeReasons'>
-            <img src={reason1} alt='reson1' />
+            <img src={reason1} alt='reson1' className={"reason-image"} />
             <span className={"reason-slogan"}>
               <h5>Playing out part in creating a digital Bhutan.</h5>
               <p>For a small country like ours, technology is the only way that we can keep up and possibly even compete with the rest of world</p>
             </span>
           </Col>
           <Col sm={4} className='threeReasons'>
-            <img src={reason2} alt='reson2' />
+            <img src={reason2} alt='reson2' className={"reason-image"} />
             <span className={"reason-slogan"}>
               <h5>His Majesty's constant push for technology</h5>
               <p>His Majesty has championed the push for technology and its importance especially in a small country like Bhutan.</p>
             </span>
           </Col>
           <Col sm={4} className='threeReasons'>
-            <img src={reason3} alt='reson3' />
+            <img src={reason3} alt='reson3' className={"reason-image"} />
             <span className={"reason-slogan"}>
               <h5>Rethinking our Education and Learning system. </h5>
               <p>The digital revolution has changed a lot of things around us. Unfortunately our education and the way we learn hasn't changed much.</p>
