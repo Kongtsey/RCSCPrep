e408e9d597d300b6bb1058122f361b4b48fecff4
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2020-08-13 20:31:25 +0600

    FEATURE: edited the founder's page

--- front-end/rcsc/src/components/foundersComponent/founders.js
+++ front-end/rcsc/src/components/foundersComponent/founders.js
@@ -10,20 +10,22 @@ class Founders extends React.Component {
       <Container fluid={true} className='foundersContainer'>
         <Row className='justify-content-center'>
           <Col sm={10} className='title'>
-            <h1>Founders</h1>
+            <h1>F O U N D E R S</h1>
           </Col>
         </Row>
         <hr className='titleHr' />
-        <Row>
-          <Col sm={3} className='foundersPicContainer'>
-            <img src={founders} className='foundersPic' alt='foundersPic' />
-          </Col>
-          <Col sm={7} className='foundersBio'>
-            RCSC Prep makes it easy for you to study at your own pace and at your own time. Our feedback is personalized to your performance and helps you find your weaknesses and your strengths. RCSC
-            Prep also has features that make you be able to take control of when you want to study and is tailor made just for you. You can also choose to focus on what you lack rather than just going
-            along with the classroom, in a sense RCSC Prep is what allows you to make this education a 21st century education and the best part is that its completely free.
-          </Col>
-        </Row>
+        <Container>
+          <Row>
+            <Col sm={3} className='foundersPicContainer'>
+              <img src={founders} className='foundersPic' alt='foundersPic' />
+            </Col>
+            <Col sm={9} className='foundersBio'>
+              RCSC Prep makes it easy for you to study at your own pace and at your own time. Our feedback is personalized to your performance and helps you find your weaknesses and your strengths.
+              RCSC Prep also has features that make you be able to take control of when you want to study and is tailor made just for you. You can also choose to focus on what you lack rather than
+              just going along with the classroom, in a sense RCSC Prep is what allows you to make this education a 21st century education and the best part is that its completely free.
+            </Col>
+          </Row>
+        </Container>
       </Container>
     );
   }
--- front-end/rcsc/src/components/homeFrontPage.js
+++ front-end/rcsc/src/components/homeFrontPage.js
@@ -2,33 +2,37 @@ import React from "react";
 import { Container, Col, Row, Button } from "react-bootstrap";
 import "../style-sheet/home-front-page.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChartPie, faLaptopHouse,faMoneyBill } from "@fortawesome/free-solid-svg-icons";
-import user from "../images/homePage/laptop.png"
+import { faChartPie, faLaptopHouse, faMoneyBill } from "@fortawesome/free-solid-svg-icons";
+import user from "../images/homePage/laptop.png";
 
 function FrontPage() {
   return (
     <React.Fragment>
       <Container className='homepage_parent_container'>
         <Row className='front_page_content'>
-          <Col lg={6} md={6} sm={12} className="tagline frontPageHeader">
-            <p className='tagline_message'>Pass the RCSC Prelims with ease!</p>
+          <Col lg={6} md={6} sm={12} className='tagline frontPageHeader'>
+            <p className='tagline_message'>
+              Pass the <b>RCSC Prelims </b>with ease!
+            </p>
             <br />
             <p className='product_description'>
               Our free online test prep makes it easier for you to practice for the P.E and to score better on it. By providing you metrics with how you're performing, RCSC Prep makes prepping for the
               Prelims more fun and efficient!
             </p>
-            <Button className="exploreButton"> Explore</Button>
+            <Button className='exploreButton'> Explore</Button>
           </Col>
           <Col lg={6} md={6} sm={12} className='front_page_image'>
             <img alt='people sitting infront of a laptop' src={user}></img>
           </Col>
         </Row>
-        <Row style={{marginBottom: "25px",marginTop: "40px"}}>
+        <Row style={{ marginBottom: "25px", marginTop: "40px" }}>
           <Col lg={4} md={4} sm={12}>
             <Row>
-              <Col sm={11} className="featureContainer">
+              <Col sm={11} className='featureContainer'>
                 <Row>
-                  <Col sm={12} className='featureIconsContainer'><FontAwesomeIcon icon={faChartPie} className='featureIcons' /></Col>
+                  <Col sm={12} className='featureIconsContainer'>
+                    <FontAwesomeIcon icon={faChartPie} className='featureIcons' />
+                  </Col>
                 </Row>
                 <p className='featureTitle'>Smart FeedBack</p> <br />
                 <ul className='featureDescription'>
@@ -40,9 +44,11 @@ function FrontPage() {
           </Col>
           <Col lg={4} md={4} sm={12}>
             <Row>
-              <Col sm={11} className="featureContainer">
+              <Col sm={11} className='featureContainer'>
                 <Row>
-                  <Col sm={12} className='featureIconsContainer'><FontAwesomeIcon icon={faLaptopHouse} className='featureIcons' /></Col>
+                  <Col sm={12} className='featureIconsContainer'>
+                    <FontAwesomeIcon icon={faLaptopHouse} className='featureIcons' />
+                  </Col>
                 </Row>
                 <p className='featureTitle'>Accessible</p> <br />
                 <ul className='featureDescription'>
@@ -53,10 +59,12 @@ function FrontPage() {
             </Row>
           </Col>
           <Col lg={4} md={4} sm={12}>
-            <Row className="justify-content-center">
-              <Col sm={11}className="featureContainer" >
+            <Row className='justify-content-center'>
+              <Col sm={11} className='featureContainer'>
                 <Row>
-                  <Col sm={12} className='featureIconsContainer'><FontAwesomeIcon icon={faMoneyBill} className='featureIcons' /></Col>
+                  <Col sm={12} className='featureIconsContainer'>
+                    <FontAwesomeIcon icon={faMoneyBill} className='featureIcons' />
+                  </Col>
                 </Row>
                 <p className='featureTitle'>Free!</p> <br />
                 <ul className='featureDescription'>
