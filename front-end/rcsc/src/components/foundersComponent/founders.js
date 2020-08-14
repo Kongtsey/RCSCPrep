5a9e31554fea97ae6c5a30328aea9c76cd97fd06
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2020-08-14 13:08:45 +0600

    ADD: finsihed the footer mobile version

--- front-end/rcsc/src/components/foundersComponent/founders.js
+++ front-end/rcsc/src/components/foundersComponent/founders.js
@@ -16,13 +16,13 @@ class Founders extends React.Component {
         <hr className='titleHr' />
         <Container>
           <Row>
-            <Col xs={12} sm={3} className='foundersPicContainer'>
-              <img src={founders} className='foundersPic' alt='foundersPic' />
-            </Col>
             <Col xs={12} sm={9} className='foundersBio'>
               RCSC Prep makes it easy for you to study at your own pace and at your own time. Our feedback is personalized to your performance and helps you find your weaknesses and your strengths.
               RCSC Prep also has features that make you be able to take control of when you want to study and is tailor made just for you. You can also choose to focus on what you lack rather than
               just going along with the classroom, in a sense RCSC Prep is what allows you to make this education a 21st century education and the best part is that its completely free.
+            </Col>
+            <Col xs={12} sm={3} className='foundersPicContainer'>
+              <img src={founders} className='foundersPic' alt='foundersPic' />
             </Col>
           </Row>
         </Container>
--- front-end/rcsc/src/components/joinUs/joinUs.js
+++ front-end/rcsc/src/components/joinUs/joinUs.js
@@ -1,49 +1,48 @@
 import React from "react";
-import { Container,Col,Row } from "react-bootstrap";
-import "./joinUs.css"
+import { Container, Col, Row } from "react-bootstrap";
+import "./joinUs.css";
 
-
-class JoinUs extends React.Component{
-    render() {
-        return (
-            <Container className="joinUsContainer" fluid={true}>
-                <Row className="justify-content-center">
-                    <Col sm={12}>
-                        <Row>
-                            <Col sm={12} className="joinUsHeader">
-                                <h3>Join Us</h3>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col sm={12} className="lookingFor">
-                                <h5>We are currently looking for: </h5>
-                            </Col>
-                        </Row>
-                        <Row className="justify-content-center">
-                            <Col sm={8} className="employeeBoxesContainer">
-                                <div className="employeeBoxes">
-                                    <p>Graphic Designer</p>
-                                </div>
-                            </Col>
-                        </Row>
-                        <Row className="justify-content-center">
-                            <Col sm={8} className="employeeBoxesContainer">
-                                <div className="employeeBoxes">
-                                    <p>Webapp Dev</p>
-                                </div>
-                            </Col>
-                        </Row>
-                        <Row className="justify-content-center" style={{marginBottom: "150px"}}>
-                            <Col sm={8} className="employeeBoxesContainer">
-                                <div className="employeeBoxes">
-                                    <p>Digital Marketer</p>
-                                </div>
-                            </Col>
-                        </Row>
-                    </Col>
-                </Row>
-            </Container>
-        )
-    }
+class JoinUs extends React.Component {
+  render() {
+    return (
+      <Container className='joinUsContainer' fluid={true}>
+        <Row className='justify-content-center'>
+          <Col sm={12}>
+            <Row>
+              <Col sm={12} className='joinUsHeader'>
+                <h3>Join Us</h3>
+              </Col>
+            </Row>
+            <Row>
+              <Col sm={12} className='lookingFor'>
+                <h5>We are currently looking for: </h5>
+              </Col>
+            </Row>
+            <Row className='justify-content-center'>
+              <Col sm={8} className='employeeBoxesContainer'>
+                <div className='employeeBoxes'>
+                  <p>Graphic Designer</p>
+                </div>
+              </Col>
+            </Row>
+            <Row className='justify-content-center'>
+              <Col sm={8} className='employeeBoxesContainer'>
+                <div className='employeeBoxes'>
+                  <p>Webapp Dev</p>
+                </div>
+              </Col>
+            </Row>
+            <Row className='justify-content-center' style={{ marginBottom: "150px" }}>
+              <Col sm={8} className='employeeBoxesContainer'>
+                <div className='employeeBoxes'>
+                  <p>Digital Marketer</p>
+                </div>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 export default JoinUs;
