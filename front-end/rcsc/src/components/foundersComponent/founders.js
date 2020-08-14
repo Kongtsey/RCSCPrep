dcae8ddeb15d87f57568c4557adf41551d2a3339
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2020-08-14 12:34:03 +0600

    ADD: finished the founders mobile page

--- front-end/rcsc/src/components/foundersComponent/founders.js
+++ front-end/rcsc/src/components/foundersComponent/founders.js
@@ -16,10 +16,10 @@ class Founders extends React.Component {
         <hr className='titleHr' />
         <Container>
           <Row>
-            <Col sm={3} className='foundersPicContainer'>
+            <Col xs={12} sm={3} className='foundersPicContainer'>
               <img src={founders} className='foundersPic' alt='foundersPic' />
             </Col>
-            <Col sm={9} className='foundersBio'>
+            <Col xs={12} sm={9} className='foundersBio'>
               RCSC Prep makes it easy for you to study at your own pace and at your own time. Our feedback is personalized to your performance and helps you find your weaknesses and your strengths.
               RCSC Prep also has features that make you be able to take control of when you want to study and is tailor made just for you. You can also choose to focus on what you lack rather than
               just going along with the classroom, in a sense RCSC Prep is what allows you to make this education a 21st century education and the best part is that its completely free.
