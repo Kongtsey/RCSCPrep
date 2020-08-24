226bbbf0f7b394cb5037429eae9fe0717959f414
Kzoeps <kzoepa@gmail.com>
2020-08-24 22:41:32 +0600

    added the pieChartPage

---
+++ front-end/rcsc/src/pages/examPracticePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SignUpExamResult from "../components/exam-on-sign-up/exam-on-sign-up-result";
+class ExamPracticePage extends React.Component{
+    render() {
+        return (
+            <div>
+                <SignUpExamResult/>
+            </div>
+        );
+    }
+}
+export default ExamPracticePage;
