a04a36efb7898163022f297f1beeada0521fe87c
Kzoeps <kzoepa@gmail.com>
2020-09-10 10:37:27 +0600

    hosting files changes

--- front-end/rcsc/src/pages/tutorialsPage/allTopicsTutorials.js
+++ front-end/rcsc/src/pages/tutorialsPage/allTopicsTutorials.js
@@ -28,7 +28,7 @@ class AllTopicsTutorials extends React.Component {
                                     <TutorialJumbotron imageAlt="golden ratio, fractals" coverImage={solvingPatterns}
                                                        pathname="/tutorial_videos/solving_patterns"
                                                        tutorialVidTitle="Solving Patterns"
-                                                       vidSrc="https://www.youtube.com/embed/0Ip6-mUSOZo"
+                                                       vidSrc="://www.youtube.com/embed/0Ip6-mUSOZo"
                                                        relatedResources="khanacademy.com,udemy.com,examveda.com"
                                                        tutorialDescrip=" Solving patterns is basically all questions that relate to finding a pattern    within a question. These type of questions are very common in the RCSC PE and has come up atleast once in the past 5 RCSC papers. These are usually easy to solve and also have a set direction you can follow. Some of eg questions are: find next letter in series: A D B C D B __"
                                                        vidTitle="Solving patterns video"/>
