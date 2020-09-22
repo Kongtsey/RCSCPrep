73c4cb6b1f720c27f6d9b90188b90ac73db9f8f7
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2020-09-22 11:52:58 +0600

    ADD: added the maintanence page

---
+++ front-end/rcsc/src/components/maintenance/maintanence.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Container, Jumbotron, Button } from "react-bootstrap";
+import "./maintanence.css";
+
+function Maintanence() {
+  return (
+    <React.Fragment>
+      <Container className={"maintanence-modal"}>
+        <Jumbotron>
+          <h1 style={{ color: "#f36767" }}>Maintanence</h1>
+          <p style={{ fontSize: "large" }}>
+            <b>
+              Currently undergoing maintanence work.
+              <br />
+            </b>
+          </p>
+          <p style={{ textAlign: "center" }}>
+            <hr />
+            <br />
+            <b>Visit us again at 2:00 pm 22st September. </b>
+            <br />
+            We apologize for the inconveneince!
+          </p>
+        </Jumbotron>
+      </Container>
+    </React.Fragment>
+  );
+}
+
+export default Maintanence;
